35c785e9577a1f9a9d868ffd9f2475389bde727e
WuDiDaBinGe <1151680016@qq.com>
2021-04-14 22:01:07 +0800

    写了几道数组timu

--- Array/Remove Duplicates from Sorted Array.cpp
+++ Array/Remove Duplicates from Sorted Array.cpp
@@ -1,9 +1,21 @@
 #include<iostream>
 #include<vector>
+#include <iterator>
+#include <cassert>
 using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
+        if(nums.empty()) return 0;
+        int index =0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[index] != nums[i]){
+                nums[++index] = nums[i];
+            }
+        }
+        return index+1;
+    }
+    int removeDuplicates1(vector<int>& nums){
+        return distance(nums.begin(),unique(nums.begin(),nums.end()));
     }
 };
