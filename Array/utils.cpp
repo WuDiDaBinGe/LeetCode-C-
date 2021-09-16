ae164d6379b25b401959dd517838fb1424dbac39
1151680016@qq.com <1151680016@qq.com>
2021-09-16 09:36:35 +0800

    有效数独

--- Array/utils.cpp
+++ Array/utils.cpp
@@ -6,3 +6,8 @@ void print_list(vector<int>& nums){
         cout<<nums[i]<<" ";
     cout<<endl;
 }
+void print_list(vector<char>& nums){
+    for (int i = 0; i < nums.size(); i++)
+        cout<<nums[i]<<" ";
+    cout<<endl;
+}
