45961d6a64d4a5af8238879ba3debbe967ed6e03
1151680016@qq.com <1151680016@qq.com>
2022-03-21 23:39:25 +0800

    编辑距离

--- dynamicProgramming/EditDistance.cpp
+++ dynamicProgramming/EditDistance.cpp
@@ -29,24 +29,30 @@ public:
         return memorizes[i][j];
     }
 };
-int minDistace(string word1, string word2) {
+int minDistance(string word1, string word2) {
     int n = word1.size();
     int m = word2.size();
     vector<vector<int>> dp = vector<vector<int>>(n + 1, vector<int>(m + 1, 0));
-    for(int i = 0; i <= m; i++) {
-        dp[i][0] = i + 1;
-        dp[0][i] = i + 1;
+    for(int i = 0; i <= n; i++) {
+        dp[i][0] = i ;
+    }
+    for(int j = 0; j <= m; j++) {
+        dp[0][j] = j;
     }
     // base case
     for(int i = 1; i<= n; i++) {
-        // for(int j = 1; j<= m; j++) {
-        //     if(word1[i] == word2[j])
-        //         dp[i][j] = dp[i - 1][j - 1];
-        //     else{
-        //         dp[i][j] = min(dp[i][]);
-        //     }
-        // }
+        for(int j = 1; j<= m; j++) {
+            if(word1[i - 1] == word2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+            else{
+                // as(s)c assd
+                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]);
+                dp[i][j] = min(dp[i - 1][j - 1], dp[i][j]);
+                dp[i][j] += 1;
+            }
+        }
     }
+    return dp[n][m];
 }
 int main(){
     return 0;
