26a7f9eaca6fba146f4dc046cc4190cf9db428e4
WuDiDaBinGe <1151680016@qq.com>
2022-03-21 22:14:34 +0800

    动规

---
+++ stack/longestValidParentheses.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+//使用栈的方法
+int longestValidParentheses(string s) {
+    stack<int> stk;
+    int last = -1;
+    int res = 0;
+    for(int i = 0;i < s.size(); i++) {
+        if(s[i] == '(') {
+            stk.push(i);
+        }
+        else {
+            // no matching left
+            if(stk.empty()) 
+                last = i;
+            // match a left 
+            else {
+                stk.pop();
+                if(stk.empty())
+                    res = max(res, i - last);
+                else
+                    res = max(res, i - stk.top());
+            }
+        }
+    }
+    return res;
+}
+//动态规划算法
+int longestValidParentheses(string s) {
+    int n = s.size();
+    int res = INT8_MIN;
+    if(n == 1 || n == 0) return 0; 
+    vector<int> dp(n + 1, 0);
+    for(int i = 2; i <= n; i++) {
+        if(s[i - 1] == '(') 
+            continue;
+        if(s[i - 2] == '(')
+            dp[i] = dp [i - 2] + 2;
+        else if(i - 2 - dp[i -1] >= 0){
+            if(s[i  - 2 - dp[i - 1]] == '(')
+                dp[i] = dp[i - 1] + 2 + dp[i - dp[i - 1] - 2];
+        }
+        res = max(res, dp[i]);
+    }
+    return res == INT8_MIN ? 0 : res;
+}
+// O(1)空间复杂度
+int main() {
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,12 +1,26 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    char name[10];
-    int a = 100;
-    a = a + a * a;
-    cout<<"Hello World"<<endl;
-    cout<<a<<endl;
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    vector<int> V(n + 1), ans(n + 1);
+    for (int i = 1; i <= n; ++i)
+        cin >> V[i];
+    stack<int> S;
+    for (int i = 1; i <= n; ++i)
+    {
+        while (!S.empty() && V[S.top()] < V[i])
+        {
+            ans[S.top()] = i;
+            cout<<V[S.top()]<<" ";
+            S.pop();
+        }
+        S.push(i);
+    }
+    cout<<endl;
+    for (int i = 1; i <= n; ++i)
+        cout << ans[i] << " ";
     return 0;
 }
-
