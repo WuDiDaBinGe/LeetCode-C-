44c3bcfbe2a1069e4ac3ad082185db0470081e00
1151680016@qq.com <1151680016@qq.com>
2022-03-31 14:18:49 +0800

    华为2022第三题完成

--- 华为2022笔试/3.cpp
+++ 华为2022笔试/3.cpp
@@ -5,6 +5,9 @@
 #include<cstdio>
 #include<unordered_map>
 using namespace std;
+// 先从满二叉树中建树 
+// 使用dfs+hashmap方式查找重复的子树
+// 判断子树层数是否为1(要求节点数>2) 使用层序遍历反序列化出满二叉树的序列
 struct TreeNode {
     int val;
     TreeNode* left;
@@ -36,7 +39,7 @@ TreeNode* deserilize(string s) {
         cout<<nums[i]<<" ";
     }
     cout<<endl;
-    for(int i = 0; i < nodes.size(); i++) {
+    for(int i = 0; i < nodes.size()/2; i++) {
         if(nums[i] > 0) {
             nodes[i]->left = 2 * i + 1 < nodes.size()?nodes[2 * i + 1]:nullptr;
             nodes[i]->right = 2 * i + 2 < nodes.size()?nodes[2 * i + 2]:nullptr;
@@ -64,27 +67,51 @@ int numNode(TreeNode* root) {
     if(root == nullptr) return 0;
     return max(numNode(root->left), numNode(root->right)) + 1;
 }
-vector<vector<int>> levelOrder(TreeNode* root) {
+string levelOrder(TreeNode* root) {
     queue<TreeNode*> q;
-    vector<vector<int>> res;
+    string res = "";
     if(root == nullptr) return res;
     q.push(root);
-    
+    int totallevel = numNode(root);
+    int level = 0;
+    while(!q.empty() && level < totallevel) {
+        int count = q.size();
+        for(int i = 0; i < count; i++) {
+            TreeNode* t = q.front(); q.pop();
+            if(t == nullptr) {
+                res += "nullptr,";
+                q.push(nullptr);
+                q.push(nullptr);
+            } else {
+                res += (to_string(t->val) + ",");
+                q.push(t->left);
+                q.push(t->right);
+            }
+        }
+        level++;
+    }
+    res.pop_back();
+    return res;
 }
+
 int main() {
     string s; cin>>s;
-    
-    for() {
-
-    }
+    // TreeNode* root = new TreeNode(1);
+    // TreeNode* r1 = new TreeNode(2);
+    // TreeNode* r2 = new TreeNode(1);
+    // TreeNode* r3 = new TreeNode(3);
+    // root->left = r1;
+    // root->right = r2;
+    // r2->right = r3;
+    // cout<<levelOrder(root)<<endl;
     TreeNode* root = deserilize(s);
     vector<TreeNode*> res = findDup(root);
     for(int i = 0; i < res.size(); i++) {
         if(numNode(res[i]) > 1) {
+            cout<<"["<<levelOrder(res[i])<<"]"<<endl;
             return 0;
         }
     }
     cout<<-1<<endl;
     return 0;
 }
-//[1,4,3,1,null,2,null]
