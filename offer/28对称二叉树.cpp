18e88e3f27df098bf100edeceb557ed9fdc810a1
WuDiDaBinGe <1151680016@qq.com>
2022-04-13 16:54:30 +0800

    复习复习

--- offer/28对称二叉树.cpp
+++ offer/28对称二叉树.cpp
@@ -15,3 +15,27 @@ bool isSym(TreeNode* a, TreeNode* b) {
     if(a->val != b->val) return false;
     return isSym(a->left, b->right) && isSym(a->right, b->left);
 }
+class Solution {
+public:
+    bool check(TreeNode *u, TreeNode *v) {
+        queue <TreeNode*> q;
+        q.push(u); q.push(v);
+        while (!q.empty()) {
+            u = q.front(); q.pop();
+            v = q.front(); q.pop();
+            if (!u && !v) continue;
+            if ((!u || !v) || (u->val != v->val)) return false;
+
+            q.push(u->left); 
+            q.push(v->right);
+
+            q.push(u->right); 
+            q.push(v->left);
+        }
+        return true;
+    }
+
+    bool isSymmetric(TreeNode* root) {
+        return check(root, root);
+    }
+};
