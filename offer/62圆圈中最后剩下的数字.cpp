52ddb5b0d1a89b5bd8391d7784dad11dffd93b16
WuDiDaBinGe <1151680016@qq.com>
2022-04-20 23:51:17 +0800

    完成剑指offer

--- offer/62圆圈中最后剩下的数字.cpp
+++ offer/62圆圈中最后剩下的数字.cpp
@@ -1,7 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+// dp
 int lastRemaining(int n, int m) {
-    
+    int dp = 0;
+    for(int i = 2; i < n; i++) {
+        dp = (dp + m) % i;
+    }
+    return dp;
 }
 int main() {
     return 0;
